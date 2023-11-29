790312cc6ab0ff3debd43879afbac1a51fe62481
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-11-28 19:31:34 -0800

    updating task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is between 97 and 122 (inclusive)
-    return 97 <= ord(c) <= 122
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
