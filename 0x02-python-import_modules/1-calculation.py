33e86ee2aeadd1324e3d8ef4dae7e0a9ed3ad7a9
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-11-30 18:23:17 -0800

    hot fix task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     b = 5
 
     from calculator_1 import add, sub, mul, div
-    print("{} + {} = {}".format(a, b, (add(a, b))))
-    print("{} - {} = {}".format(a, b, (sub(a, b))))
-    print("{} * {} = {}".format(a, b, (mul(a, b))))
-    print("{} / {} = {}".format(a, b, (div(a, b))))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
