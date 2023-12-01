d1dd73721c6fadb41fa3c8d6d0830ab40ea31d22
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-11-30 17:54:54 -0800

    hot fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-# Assign values to variables
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-# Import the add function from add_0.py
-from add_0 import add
+    from add_0 import add
 
-# Call the add function with the assigned variables
-result = add(a, b)
-
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
