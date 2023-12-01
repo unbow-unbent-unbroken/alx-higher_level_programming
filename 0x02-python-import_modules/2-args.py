86bbd7b3c5d07626b6af7c5a7316618a2ae9084f
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-11-30 18:54:00 -0800

    updating task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from sys import argv
+
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+
+    elif num_args == 1:
+        print("1 argument:")
+
+    else:
+        print("{} arguments".format(num_args))
+
+    for i in range(num_args):
+        print("{}: {}".format(i + 1, argv[i + 1]))
