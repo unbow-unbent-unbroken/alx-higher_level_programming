2972f76cb8f1808fd626d3f85341f89a56e8fe76
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 07:27:57 -0800

    hot fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_index, element in enumerate(row):
             if col_index == len(row) - 1:
-                print("{:d}".format(element))
+                print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element), end=" ")
         print()
