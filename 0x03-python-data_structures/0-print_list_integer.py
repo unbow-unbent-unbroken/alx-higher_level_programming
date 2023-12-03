5f47a13ad87be116c3f918a3e08c94b406d158ba
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 03:06:43 -0800

    hot fix function

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def print_list_integer(mu_list=[]):
+def print_list_integer(my_list=[]):
     for integers in my_list:
-        print('{:d}'.format(integer))
+        print("{:d}".format(integer))
