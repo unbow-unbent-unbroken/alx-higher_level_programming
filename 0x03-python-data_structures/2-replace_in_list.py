1219c6109506888ba6dec28c40b30066853b602c
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 03:29:29 -0800

    hot fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list
+        return (my_list)
     elif idx >= len(my_list):
-        return my_list
+        return (my_list)
     else:
-        return my_list[idx] = element
+        return (my_list[idx] = element)
