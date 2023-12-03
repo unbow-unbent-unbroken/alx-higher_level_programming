aef3b6c54e436b7676ec20eca0dfd7216f09f976
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 07:28:41 -0800

    still fixing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element), end=" ")
-        print()
