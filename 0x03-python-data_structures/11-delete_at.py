2e59f872d4916d90cafed65ca0138b5469095948
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 08:32:02 -0800

    another hot fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del(my_list[idx])
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
-    return (my_list)
+    new_list = my_list[:idx] + my_list[idx + 1:]
+    return new_list
