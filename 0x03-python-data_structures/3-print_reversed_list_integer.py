c8fedd6126d45ad23759e43f622d850204b1962a
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 03:51:16 -0800

    hot fix by including if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for integer in reversed(my_list):
-        print("{:d}".format(interger))
+    if my_list:
+        for integer in reversed(my_list):
+            print("{:d}".format(interger))
