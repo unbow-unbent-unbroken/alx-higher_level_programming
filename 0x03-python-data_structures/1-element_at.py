56c69394951cfd1f9dd0d7e5e59d628aa619275b
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 03:15:37 -0800

    hot fix return statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return 0
+        return None
     elif idx >= len(my_list):
-        return 0
+        return None
     else:
         return my_list[idx]
