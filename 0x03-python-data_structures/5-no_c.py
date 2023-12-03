ba5d5eaadabd7c9089308a9fc332c6c7138c8485
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-03 04:39:30 -0800

    argumenting the if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_string = ""
     for x in my_string:
-        if x != c and x != C:
+        if x != "c" and x != "C":
             new_string += x
         else:
             return (new_string)
