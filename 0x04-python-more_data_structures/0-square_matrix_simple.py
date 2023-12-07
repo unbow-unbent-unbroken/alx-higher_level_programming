e96a525d1cb80a5b1de8b5f36a2729cd15709c28
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-07 09:08:07 -0800

    another approach for task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result_matrix = [list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))]
-    return result_matrix
+    return [list(map(lambda y: y ** 2, row)) for row in matrix]
