fd1d527a55f65eb5a7b19e6d52cd69d5ce51f0cb
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-07 09:04:15 -0800

    hot fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result_matrix = list(map(lambda row: list(map(lambda num: num**2, row)), matrix))
+    result_matrix = list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))
     return result_matrix
