39cf079ae43b4db85d96195c0512150dd481397b
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-08 03:30:43 -0800

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
     result = 0
-    prev_value = 0
-
-    for char in reversed(roman_string):
-        value = roman_dict[char]
-        if value < prev_value:
-            result -= value
-        else:
-            result += value
-        prev_value = value
-
+    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = roman_dict[roman]
+        result += arabic if result < arabic * 5 else -arabic
     return result
