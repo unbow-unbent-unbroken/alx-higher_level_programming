5df14eaea9d1aecb7f9c46231a7ad2e38438467f
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-19 19:23:46 -0800

    correcting the documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-class Square
+class Square:
     """
-    This is an empty class that defines a square.
+    Empty class Square  that defines a square.
     A square is a flat shape that has four(4) equal sides
     """
 
