37217b10949ba96e63138ff24b6371636f57bb0c
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-08 19:14:57 -0800

    updaing task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+    - obj: The object to inspect.
+
+    Returns:
+    - A list containing the attributes and methods of the object.
+    """
+    return (dir(obj))
+
