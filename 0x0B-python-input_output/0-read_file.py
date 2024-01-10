a9fe07afae12d8a23cbfbd5edc4a6aa2862940b8
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 18:45:27 -0800

    MSG is good

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,14 +2,6 @@
 """Module Documentation: This  module define a function that reads text file."""
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-
-    Returns:
-        None
-    """
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
