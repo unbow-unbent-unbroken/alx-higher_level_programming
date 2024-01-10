b2f9396e91a4b93d672e15c0bc29e37fd4f57211
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 18:59:27 -0800

    updating task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Documentation: This module contains a function that writes a string to a text file
+    and return the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a text file (UTF8) to stdout."""
+    with open(filename, w) as f:
+        f.write(text)
+
+
+
