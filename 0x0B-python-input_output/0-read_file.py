c93fe2a85a446cb66df1bf2a43d637de210a7de9
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 18:42:55 -0800

    fuiyooh

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Module Documentation: This  module define a function that reads text file.
-"""
+"""Module Documentation: This  module define a function that reads text file."""
 
 def read_file(filename=""):
     """
