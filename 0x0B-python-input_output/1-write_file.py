618f0c7e028493ef72e4f921492eff5b22994e06
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-10 08:38:26 -0800

    hot fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""This module contains a function that writes a string to a text file."""
+"""This module defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """Writes a text file (UTF8) to stdout.
+    """Writes a string to a UTF8 text file
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
-
-
