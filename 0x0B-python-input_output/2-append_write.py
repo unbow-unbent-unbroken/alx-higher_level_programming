26a303d015733f9d569cd3d9c5084126c2eba002
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-10 08:40:33 -0800

    hot fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module contains a function that appends a string at the end of a text file"""
+"""This module defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
