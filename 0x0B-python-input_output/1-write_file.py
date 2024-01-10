eeb64296c919268d0e10ea9df865a1a1a5b15322
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 19:06:49 -0800

    include return statement

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,8 +8,7 @@ Module Documentation: This module contains a function that writes a string to a 
 def write_file(filename="", text=""):
     """Writes a text file (UTF8) to stdout."""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
-        f.close()
+        return f.write(text)
 
 
 
