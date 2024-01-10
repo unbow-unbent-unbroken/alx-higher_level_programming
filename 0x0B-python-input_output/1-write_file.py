eb2d6d00c29e1ea94c44bb75f811b80dd73406c4
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 19:01:58 -0800

    hot fix 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,7 +8,8 @@ Module Documentation: This module contains a function that writes a string to a 
 def write_file(filename="", text=""):
     """Writes a text file (UTF8) to stdout."""
     with open(filename, w) as f:
-        print(f.write(text), end"")
+        f.write(text)
+        f.close()
 
 
 
