6424cb14550b4730f45d18b877d08831f1e37e3b
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 18:47:46 -0800

    checking pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This  module define a function that reads text file."""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
