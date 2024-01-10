e220911076d23c6f1ad60fed3bc3fd960fdc5a76
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 19:11:21 -0800

    i would loose my mind

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a text file (UTF8) to stdout."""
+    """Writes a text file (UTF8) to stdout.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
 
