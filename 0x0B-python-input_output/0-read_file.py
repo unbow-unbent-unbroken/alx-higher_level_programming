affdeb1969f1f573fea51d2fdb77dde761feb083
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-09 18:34:32 -0800

    hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """
-Module Documentation: This 
+Module Documentation: This  module define a function that reads text file.
 """
 
 def read_file(filename=""):
-    with open('filename', encoding="utf-8")
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        None
+    """
+    with open('filename', encoding="utf-8") as file:
+        for line in file:
+            print(line, end='')
