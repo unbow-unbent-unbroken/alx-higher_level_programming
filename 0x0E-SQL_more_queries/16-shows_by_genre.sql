46a69e4b422a7b75cbbab72e9bfc12bcdbfc0c33
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-02-16 10:24:06 -0800

    hot fix 2

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,15 +1,13 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows.
--- The tv_genres table contains only one record where name = Comedy (but the id can be different)
--- Each record should display: tv_shows.title
--- Results must be sorted in ascending order by the show title
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
