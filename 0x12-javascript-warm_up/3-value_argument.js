b5e50f976909e3e8ce1444b77ed814492b044e23
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-12 21:16:25 +0100

    updating task 4 file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
 const firstArg = process.argv[2];
 
 if (firstArg === undefined) {
-    console.log('No argument')
+    console.log('No argument');
 }
 else {
-    console.log(firstArg)
+    console.log(firstArg);
 }
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+if (firstArg === 1 & secondArg === 2)
+{
+    console.log(firstArg, "is", secondArg);
+}
+else if (firstArg === 1 & secondArg === undefined)
+{
+    console.log(firstArg, "is", secondArg)
+}
+else if (firstArg === undefined & secondArg === 2)
+{
+    console.log(firstArg, "is", secondArg)
+}
+else
+{
+    console.log(firstArg, "is", secondArg)
+}
