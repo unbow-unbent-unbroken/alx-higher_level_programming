d0e02f9f9d2426c9e83f2a878506353f385fea74
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-13 03:16:53 +0100

    semistandard compliance

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,9 @@
 
 const myArg = process.argv.slice(2);
 
-if (myArg.length === 0 || myArg.length === 1)
-{
-    console.log(0);
-}
-else
-{
-    const num = myArg.map(myArg => parseInt(myArg)).sort((a, b) => b - a);
-    console.log(num[1]);
+if (myArg.length === 0 || myArg.length === 1) {
+  console.log(0);
+} else {
+  const num = myArg.map(myArg => parseInt(myArg)).sort((a, b) => b - a);
+  console.log(num[1]);
 }
