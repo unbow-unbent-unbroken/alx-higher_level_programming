9e55950b0b00ad4e2faa837d0f4ffdeec2c9f526
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-13 01:15:55 +0100

    hot fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
 const arrayOfString = ['C is fun', 'Python is cool', 'JavaScript is amazing',];
-
+for (const i of arrayOfString)
+{
+    console.log(i);
+}
+/*
 for (i = 0; i < arrayOfString.length; i++)
 {
     console.log(arrayOfString[i]);
 }
+*/
