97ca56bb3e28d5f454d7abdf79742d730ce6cbbb
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-14 01:06:04 +0100

    updating task 1 file

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ * Define a REctangle class
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    this.w = width;
+    this.h = height;
+  }
+}
+
+module.exports = Rectangle;
