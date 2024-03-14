eeae72b7d79554605d0b784d7c38cf5995ecfdfa
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-14 03:01:05 +0100

    updating task 4 file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+/**
+ * Defines a Rectangle
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    console.log(this.height * 2, this.width * 2);
+  }
+}
+module.exports = Rectangle;
