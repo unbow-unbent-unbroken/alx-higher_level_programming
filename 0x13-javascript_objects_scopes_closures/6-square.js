31573744b8e317cfcbf7cc40b47b736a190acf6c
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-14 03:41:06 +0100

    😈👹👺🫣  oops lets see

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+/**
+ * A Square class that inherits from 5-square.js
+ */
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+    constructor (size) {
+        super (size);
+    }
+    charPrint (c) {
+        if (c === undefined)
+        {
+            c = "X";
+        }
+    }
+    for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+    }
+}
+module.exports = Square;
