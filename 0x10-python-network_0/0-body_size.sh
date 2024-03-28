9f488ac5a56a792e747dbfe82e7ad6027fb54926
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-03-28 03:53:05 +0100

    👨🏾‍💻✅🤙🏾👍🏾 hot fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-
+# send a request to an URL with curl, and displays the size of the body of the response
 curl -s "$1" | wc -c
