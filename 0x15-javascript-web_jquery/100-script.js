9b0d756ccd0b7229a9c46e10f09401d83928bb65
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-05-02 18:39:40 +0100

    making files executable and updating adv tasks

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the <header> element
+  const headerElement = document.querySelector('header');
+
+  // Update the text color to red
+  headerElement.style.color = '#FF0000';
+});
